eb9cfb252af1cd822a0f047711a11151813e7781
Abhijit Majumdar <Abhijit.Majumdar@cognizant.com>
2018-11-11 12:47:42 +0000

    Initial Load

---
+++ ForexService/src/main/java/com/test/microservice/forex/ForexService/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.test.microservice.forex.ForexService;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+
+	ExchangeValue findByFromAndTo(String from, String to);
+	
+}
